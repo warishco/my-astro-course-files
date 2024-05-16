c5d8f2698c5f7a7a91a4b4f3eeea6fcf828df1e4
Chris Pennington <chris@codinginpublic.dev>
2024-05-16 11:42:24 -0600

    lesson-99

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,22 @@
 import { defineMiddleware } from "astro:middleware";
 
-export const onRequest = defineMiddleware(async ({ url }, next) => {
-  if (url.pathname === "/") {
-    return Response.redirect(new URL("/redirected", url), 302);
-  }
+export const onRequest = defineMiddleware(async (context, next) => {
+  // if (url.pathname === "/") {
+  //   return Response.redirect(new URL("/redirected", url), 302);
+  // }
+  // return next();
+  context.locals.userMessage = () => {
+    // pathname check
+    const pathname = context.url.pathname;
+    // auth status
+    const authStatus = true;
+    if (pathname === "/" && !authStatus) {
+      return "You must be logged in.";
+    }
+    if (pathname !== "/") {
+      return "Freedom!";
+    }
+    return "";
+  };
   return next();
 });
