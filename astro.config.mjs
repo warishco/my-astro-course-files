5460a369b745cc39d07ac1728cbe8905450f85fd
Chris Pennington <chris@codinginpublic.dev>
2024-05-16 07:26:53 -0600

    lesson-97-prep

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,4 @@
-import { defineConfig } from "astro/config";
-import icon from "astro-icon";
-import tailwind from "@astrojs/tailwind";
-import netlify from "@astrojs/netlify";
-
-import db from "@astrojs/db";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  integrations: [icon(), tailwind(), db()],
-  output: "server",
-  adapter: netlify()
-});
+export default defineConfig({});
